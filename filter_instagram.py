6c4816a4d805d2f334f41b008e0ef0aebec0cf52
Anton Matveev <amatveev@avain.intra>
2018-05-28 14:27:03 +0300

    Hashtag extraction improved

--- filter_instagram.py
+++ filter_instagram.py
@@ -1,12 +1,19 @@
 # This script performs preliminary filtering of the original Instagram data set
 import pandas as pd
 import csv
+from string import punctuation
+
+def extract_hashtag(caption_part, delimiters):
+    part = caption_part
+    for delimiter in delimiters:
+        part = part.split(delimiter)[0]
+    return part
 
 print 'Importing the file'
 df = pd.read_csv('geosome-instagram.tsv', sep='\t')
 
-df = df.drop(columns=['hashtags', 'photourl'])
-print 'Removed hashtagsand photourl columns'
+df = df.drop(columns=['photourl'])
+print 'Removed the photourl column'
 
 df = df[df.text != '\N']
 print 'Removed posts without captions'
@@ -17,22 +24,23 @@ print 'Extracting hashtags'
 hashtags = []
 rows_count = float(len(df))
 percentage = 0
+delimiters = ' ' + punctuation.replace('#', '')
 print str(percentage) + '%'
 for index, row in df.iterrows():
-    caption = row['text']
+    caption = row['text'].replace('@', '')
     parts = caption.split('#')
-    parts.pop(0)
-    hashtags_string = ''
-    for part in parts:
-        hashtags_string += part.split(' ')[0] + ';'
-    hashtags.append(hashtags_string[:-1])
+    local_hashtags = []
+    for part in parts[1:]:
+        local_hashtags.append(extract_hashtag(part, delimiters))
+    caption = ''.join(parts)
+    for hashtag in local_hashtags:
+        caption = caption.replace(hashtag, '')
+    df.at[index, 'text'] = caption
+    df.at[index, 'hashtags'] = ';'.join(local_hashtags)
     new_percentage = int(index/rows_count * 100)
     if new_percentage != percentage:
         percentage = new_percentage
         print str(percentage) + '%'
-
-df['hashtags'] = pd.Series(hashtags, index=df.index)
-print 'Added a hashtags column'
 
 print 'Saving to a file'
 df.to_csv('instagram.tsv', sep='\t', quoting=csv.QUOTE_NONNUMERIC, index=False)
